a6cdb6f3a07bcb85e23d826af313857941724b2f
Danilo Tuler <danilo@cartesi.io>
2020-12-07 15:29:23 -0500

    Apollo connection per chainId

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -1,38 +1,35 @@
+// Copyright (C) 2020 Cartesi Pte. Ltd.
+
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
 import { useMemo } from 'react';
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
-import getConfig from '../../next.config';
+const uris = {
+    1: 'https://api.thegraph.com/subgraphs/name/cartesi/pos',
+    5: 'https://api.thegraph.com/subgraphs/name/cartesi/pos-goerli',
+    31337: 'https://api.thegraph.com/subgraphs/name/cartesi/pos-goerli',
+};
 
-export let apolloClient: ApolloClient<any>;
-
-const createApolloClient = (): ApolloClient<any> => {
-    const { graphUrl } = getConfig();
-
+export const initializeApollo = (chainId: number): ApolloClient<any> => {
+    const uri = uris[chainId];
     return new ApolloClient({
         ssrMode: typeof window === 'undefined',
         link: new HttpLink({
-            uri: graphUrl,
+            uri,
         }),
         cache: new InMemoryCache(),
     });
 };
 
-export const initializeApollo = (initialState = null): ApolloClient<any> => {
-    const _apolloClient = apolloClient ?? createApolloClient();
-
-    if (initialState) {
-        const existingCache = _apolloClient.extract();
-        _apolloClient.cache.restore({ ...existingCache, ...initialState });
-    }
-
-    if (typeof window === 'undefined') return _apolloClient;
-
-    if (!apolloClient) apolloClient = _apolloClient;
-
-    return _apolloClient;
-};
-
-export const useApollo = (initialState): ApolloClient<any> => {
-    const store = useMemo(() => initializeApollo(initialState), [initialState]);
+export const useApollo = (chainId: number): ApolloClient<any> => {
+    const store = useMemo(() => initializeApollo(chainId), [chainId]);
     return store;
 };
