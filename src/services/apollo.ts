45506e48347e455318f62740f39f69bb2ad08722
Danilo Tuler <danilo@cartesi.io>
2021-01-05 01:21:32 -0500

    Switching default backend to staging

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -42,7 +42,7 @@ const mergeUniqueSort = (fieldName: string) => {
 
 const createApollo = (chainId: number): ApolloClient<any> => {
     const uri =
-        uris[chainId] || 'https://api.thegraph.com/subgraphs/name/cartesi/pos';
+        uris[chainId] || 'https://api.thegraph.com/subgraphs/name/cartesi/pos-staging';
     const ssrMode = typeof window === 'undefined';
     return new ApolloClient({
         ssrMode,
