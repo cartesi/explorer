5fbb89a0799c98f7378bac58b71e1ceebd69ecc3
Alexander Bai <alexander.bai@cartesi.io>
2021-01-07 11:56:03 -0500

    Updated tinygraph url

--- src/utils/tinygraph.ts
+++ src/utils/tinygraph.ts
@@ -25,5 +25,5 @@ export const themes = [
 
 export const tinyGraphUrl = (block: Block): string => {
     const themeId = parseInt(block.chain.id) % themes.length;
-    return `https://www.tinygraphs.com/labs/isogrids/hexa/${block.producer.id}?theme=${themes[themeId]}&numcolors=4&size=220&fmt=svg`;
+    return `http://tinygraphs-cartesi.herokuapp.com/labs/isogrids/hexa/${block.producer.id}?theme=${themes[themeId]}&numcolors=4&size=220&fmt=svg`;
 };
