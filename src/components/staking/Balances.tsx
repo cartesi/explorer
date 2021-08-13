d6ab924d5ae3835ed53ecedb5ac32299cb8c51a0
Neven Dyulgerov <neven.diulgerov@gmail.com>
2021-08-13 16:58:46 +0300

    Use TransactionFeedback instead / refactoring (#252)

--- src/components/staking/Balances.tsx
+++ src/components/staking/Balances.tsx
@@ -1,73 +1,64 @@
-import React, { memo } from 'react';
+import React from 'react';
 import { Box, Flex, FlexProps, Heading, Spacer, Text } from '@chakra-ui/react';
 import { FaCoins, FaWallet } from 'react-icons/fa';
-import { BigNumberish } from 'ethers';
-import ConfirmationIndicator from '../ConfirmationIndicator';
 import CTSIText from '../CTSIText';
+import { useWeb3React } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import { useBlockNumber } from '../../services/eth';
+import { useStaking } from '../../services/staking';
+import { useCartesiToken } from '../../services/token';
+import TransactionFeedback from '../TransactionFeedback';
 
-interface BalancesProps extends FlexProps {
-    balance: BigNumberish;
-    stakedBalance: BigNumberish;
-    waiting: boolean;
-    error?: string;
-}
+export const Balances: React.FunctionComponent<FlexProps> = (props) => {
+    const { account } = useWeb3React<Web3Provider>();
+    const blockNumber = useBlockNumber();
+    const { staking, stakedBalance, transaction } = useStaking(account);
+    const { balance } = useCartesiToken(account, staking?.address, blockNumber);
 
-const areEqual = (prevProps: BalancesProps, nextProps: BalancesProps) =>
-    prevProps.waiting === nextProps.waiting &&
-    prevProps.error === nextProps.error &&
-    prevProps.balance === nextProps.balance &&
-    prevProps.stakedBalance === nextProps.stakedBalance;
+    return (
+        <Flex
+            {...props}
+            direction={['column', 'column', 'column', 'row']}
+            bg="black"
+            color="white"
+            opacity={0.87}
+            p="50px 6vw 65px 6vw"
+        >
+            <Box pb={[4, 4, 4, 0]}>
+                <Heading as="h5" size="lg" fontWeight="normal">
+                    Staking
+                </Heading>
 
-export const Balances: React.FunctionComponent<BalancesProps> = memo(
-    (props) => {
-        const { waiting, error, balance, stakedBalance, ...restProps } = props;
+                <TransactionFeedback transaction={transaction} />
+            </Box>
 
-        return (
-            <Flex
-                {...restProps}
-                direction={['column', 'column', 'column', 'row']}
-                bg="black"
-                color="white"
-                opacity={0.87}
-                p="50px 6vw 65px 6vw"
-            >
-                <Box pb={[4, 4, 4, 0]}>
-                    <Heading as="h5" size="lg" fontWeight="normal">
-                        Staking
-                    </Heading>
+            <Spacer />
 
-                    <ConfirmationIndicator loading={waiting} error={error} />
+            <Flex w={['100%', '100%', '100%', '50%']}>
+                <Box w="50%">
+                    <CTSIText
+                        value={balance}
+                        icon={FaWallet}
+                        bg="black"
+                        color="white"
+                    >
+                        <Text>Wallet Balance</Text>
+                    </CTSIText>
                 </Box>
 
-                <Spacer />
-
-                <Flex w={['100%', '100%', '100%', '50%']}>
-                    <Box w="50%">
-                        <CTSIText
-                            value={balance}
-                            icon={FaWallet}
-                            bg="black"
-                            color="white"
-                        >
-                            <Text>Wallet Balance</Text>
-                        </CTSIText>
-                    </Box>
-
-                    <Box w="50%">
-                        <CTSIText
-                            value={stakedBalance}
-                            icon={FaCoins}
-                            bg="black"
-                            color="white"
-                        >
-                            <Text>Staked Balance</Text>
-                        </CTSIText>
-                    </Box>
-                </Flex>
+                <Box w="50%">
+                    <CTSIText
+                        value={stakedBalance}
+                        icon={FaCoins}
+                        bg="black"
+                        color="white"
+                    >
+                        <Text>Staked Balance</Text>
+                    </CTSIText>
+                </Box>
             </Flex>
-        );
-    },
-    areEqual
-);
+        </Flex>
+    );
+};
 
 export default Balances;
