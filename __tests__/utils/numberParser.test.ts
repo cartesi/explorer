6b4eb1a83e0f6e44055d76601c97b4e027a78af8
Bruno Menezes <bruno.menezes@cartesi.io>
2022-05-18 17:40:37 +1200

    Added test cases for numberParser and added function overload

--- __tests__/utils/numberParser.test.ts
+++ __tests__/utils/numberParser.test.ts
@@ -1,5 +1,4 @@
 // Copyright (C) 2022 Cartesi Pte. Ltd.
-
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
 // Foundation, either version 3 of the License, or (at your option) any later
@@ -8,9 +7,33 @@
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 // PARTICULAR PURPOSE. See the GNU General Public License for more details.
+import { ethers } from 'ethers';
+import { toBigNumber } from '../../src/utils/numberParser';
 
 describe('NumberParser functions', () => {
-    it('tests to be implemented', () => {
-        expect(true).toBe(false);
+    it('should parse a number as string to ethers BigNumber', () => {
+        const numberAsString = '3999999';
+        expect(toBigNumber(numberAsString)).toEqual(
+            ethers.utils.parseUnits(numberAsString, 18)
+        );
+    });
+
+    it('should parse a number to ethers BigNumber equivalent', () => {
+        const number = 1000.5;
+        expect(toBigNumber(number)).toEqual(
+            ethers.utils.parseUnits(number.toString(), 18)
+        );
+    });
+
+    it('should throw an error when passing wrong type as value', () => {
+        try {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            toBigNumber(new Date());
+        } catch (e) {
+            expect(e.message).toEqual(
+                'Supported value types are [ string, number ]'
+            );
+        }
     });
 });
--- src/utils/numberParser.ts
+++ src/utils/numberParser.ts
@@ -8,7 +8,14 @@
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 // PARTICULAR PURPOSE. See the GNU General Public License for more details.
-import { ethers } from 'ethers';
+import { ethers, BigNumber } from 'ethers';
 
-export const toBigNumber = (value: string, decimals = 18) =>
-    ethers.utils.parseUnits(value, decimals);
+const parse = (v: string, d: number) => ethers.utils.parseUnits(v, d);
+
+export function toBigNumber(v: string, decimals?: number): BigNumber;
+export function toBigNumber(v: number, decimals?: number): BigNumber;
+export function toBigNumber(v: unknown, decimals = 18): BigNumber {
+    if (typeof v === 'string') return parse(v, decimals);
+    else if (typeof v === 'number') return parse(v.toString(), decimals);
+    else throw new Error(`Supported value types are [ string, number ]`);
+}
