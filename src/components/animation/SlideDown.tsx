4aa4c6989b496e9adebd06ae3611ae17c4f55411
Bruno Menezes <bruno.menezes@cartesi.io>
2022-06-01 11:51:57 +1200

    Added slide-down-up animation component

---
+++ src/components/animation/SlideDown.tsx
@@ -0,0 +1,48 @@
+// Copyright (C) 2022 Cartesi Pte. Ltd.
+
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+import { memo, FC } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+export interface SlideDownProps {
+    display: boolean;
+}
+
+export const SlideDown: FC<SlideDownProps> = memo(({ display, children }) => {
+    const transition = {
+        duration: 0.5,
+        bounce: 0.5,
+        type: 'spring',
+    };
+
+    return (
+        <AnimatePresence>
+            {display && (
+                <motion.div
+                    initial={{ height: 0 }}
+                    animate={{ height: 'auto' }}
+                    exit={{
+                        overflow: 'hidden',
+                        height: 0,
+                        transition: {
+                            type: 'tween',
+                            ease: 'anticipate',
+                            duration: 0.5,
+                        },
+                    }}
+                    transition={transition}
+                >
+                    {children}
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
+});
