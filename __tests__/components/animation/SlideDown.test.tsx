b064885f2a1de55975559cd10c8a8cb2c47698dd
Bruno Menezes <bruno.menezes@cartesi.io>
2022-06-01 13:24:23 +1200

    Copyright added and created test for SlideDown component

---
+++ __tests__/components/animation/SlideDown.test.tsx
@@ -0,0 +1,43 @@
+// Copyright (C) 2022 Cartesi Pte. Ltd.
+
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+import { cleanup, render, screen } from '@testing-library/react';
+import React from 'react';
+import { SlideDown } from '../../../src/components/animation/SlideDown';
+
+describe('SlideDown component', () => {
+    const DummyComponent = ({ text }) => <h1>{text}</h1>;
+    afterEach(() => {
+        cleanup();
+    });
+
+    it('should be able to render children component when display prop is true', () => {
+        render(
+            <SlideDown display={true}>
+                <DummyComponent text="Create your own pool" />
+            </SlideDown>
+        );
+
+        expect(screen.queryByText('Create your own pool')).toBeInTheDocument();
+    });
+
+    it('should not render the children component when display prop is false', () => {
+        render(
+            <SlideDown display={false}>
+                <DummyComponent text="This text should not be displayed" />
+            </SlideDown>
+        );
+
+        expect(
+            screen.queryByText('This text should not be displayed')
+        ).not.toBeInTheDocument();
+    });
+});
