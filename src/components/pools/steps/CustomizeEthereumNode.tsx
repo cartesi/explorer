af56957d3e20fc052ae61a702e3c8d12c15d4efd
Bruno Menezes <bruno.menezes@cartesi.io>
2022-06-08 17:12:54 +1200

    Reusing the customize and set-up steps from node. Using a facade like component to export the content.

---
+++ src/components/pools/steps/CustomizeEthereumNode.tsx
@@ -0,0 +1,17 @@
+// Copyright (C) 2022 Cartesi Pte. Ltd.
+
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+/**
+ * As this pool creation shares the same wording and structure as the private node creation
+ * we are just exporting directly from here, but it creates a facade. In case anything change we
+ * can modify here without touching whoever is using this component.
+ */
+export { default } from '../../node/steps/CustomizeEthereumNode';
