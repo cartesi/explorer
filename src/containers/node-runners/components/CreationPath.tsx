e8755dc3a4341823d3fd095bc3c46e73e4eef745
Bruno Menezes <bruno.menezes@cartesi.io>
2022-07-11 15:26:01 +1200

    making sure the creation-path component is removed when has both pool and node. Also added a animated transition to it.

--- src/containers/node-runners/components/CreationPath.tsx
+++ src/containers/node-runners/components/CreationPath.tsx
@@ -9,13 +9,14 @@
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 // PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-import { Heading, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Heading, Slide, useColorModeValue, VStack } from '@chakra-ui/react';
 import { useAtom } from 'jotai';
 import { NextRouter } from 'next/router';
+import { SlideDown } from '../../../components/animation/SlideDown';
 import { Card } from '../../../components/Card';
 import { AllowanceIcon, WalletIcon } from '../../../components/Icons';
 import { OrderedContent } from '../../../components/OrderedContent';
-import { isPoolManagerAtom } from '../atoms';
+import { hasPrivateNodeAtom, hasPoolsAtom } from '../atoms';
 import Block from './Block';
 
 const mainResponsability = 'Main responsabilities:';
@@ -32,59 +33,83 @@ const publicPoolResponsabilities = [
 type CreationPathProps = { router: NextRouter };
 const CreationPath = ({ router }: CreationPathProps) => {
     const bg = useColorModeValue('gray.80', 'header');
-    const [isPoolManager] = useAtom(isPoolManagerAtom);
+    const [hasPools] = useAtom(hasPoolsAtom);
+    const [hasPrivateNode] = useAtom(hasPrivateNodeAtom);
+    const noPoolsOrNodes = !hasPools || !hasPrivateNode;
 
     return (
-        <Block bg={bg}>
-            <Heading
-                fontSize="2xl"
-                mt={5}
-                mb={{ base: 4, md: 8 }}
-                fontWeight="medium"
-                lineHeight={6}
-            >
-                Create a node or pool in steps
-            </Heading>
-            <VStack align="stretch" px={{ base: '3vw', md: '9vw' }}>
-                {!isPoolManager && (
-                    <Card
-                        id="pool-creation-card"
-                        title="Create a public pool"
-                        subtitle="Earn commissions out of the blocks rewards."
-                        iconBg="yellow.100"
-                        icon={<AllowanceIcon color="yellow.500" w={6} h={6} />}
-                        buttonText={'CREATE PUBLIC POOL'}
-                        onButtonClick={() => {
-                            router.push('/pools/new');
-                        }}
-                        tooltip={
-                            <OrderedContent
-                                title={mainResponsability}
-                                orderedItems={publicPoolResponsabilities}
+        <>
+            {noPoolsOrNodes && (
+                <Block bg={bg}>
+                    <Heading
+                        fontSize="2xl"
+                        mt={5}
+                        mb={{ base: 4, md: 8 }}
+                        fontWeight="medium"
+                        lineHeight={6}
+                    >
+                        Create a node or pool in steps
+                    </Heading>
+                    <VStack align="stretch" px={{ base: '3vw', md: '9vw' }}>
+                        <SlideDown display={!hasPools}>
+                            <Card
+                                id="pool-creation-card"
+                                title="Create a public pool"
+                                subtitle="Earn commissions out of the blocks rewards."
+                                iconBg="yellow.100"
+                                icon={
+                                    <AllowanceIcon
+                                        color="yellow.500"
+                                        w={6}
+                                        h={6}
+                                    />
+                                }
+                                buttonText={'CREATE PUBLIC POOL'}
+                                onButtonClick={() => {
+                                    router.push('/pools/new');
+                                }}
+                                tooltip={
+                                    <OrderedContent
+                                        title={mainResponsability}
+                                        orderedItems={
+                                            publicPoolResponsabilities
+                                        }
+                                    />
+                                }
                             />
-                        }
-                    />
-                )}
+                        </SlideDown>
 
-                <Card
-                    id="private-node-creation-card"
-                    title="Run a private node"
-                    subtitle="You are able to stake directly by running your own node to represent your stake."
-                    iconBg="yellow.100"
-                    icon={<WalletIcon color="yellow.500" w={6} h={6} />}
-                    buttonText={'CREATE MY NODE'}
-                    onButtonClick={() => {
-                        router.push('/node/new');
-                    }}
-                    tooltip={
-                        <OrderedContent
-                            title={mainResponsability}
-                            orderedItems={privateNodeResponsabilities}
-                        />
-                    }
-                />
-            </VStack>
-        </Block>
+                        <SlideDown display={!hasPrivateNode}>
+                            <Card
+                                id="private-node-creation-card"
+                                title="Run a private node"
+                                subtitle="You are able to stake directly by running your own node to represent your stake."
+                                iconBg="yellow.100"
+                                icon={
+                                    <WalletIcon
+                                        color="yellow.500"
+                                        w={6}
+                                        h={6}
+                                    />
+                                }
+                                buttonText={'CREATE MY NODE'}
+                                onButtonClick={() => {
+                                    router.push('/node/new');
+                                }}
+                                tooltip={
+                                    <OrderedContent
+                                        title={mainResponsability}
+                                        orderedItems={
+                                            privateNodeResponsabilities
+                                        }
+                                    />
+                                }
+                            />
+                        </SlideDown>
+                    </VStack>
+                </Block>
+            )}
+        </>
     );
 };
 
