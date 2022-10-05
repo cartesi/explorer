b6b1faf68f7cc17182f4ca15d5ce9411a49c0f95
Neven Dyulgerov <neven.diulgerov@gmail.com>
2022-10-06 01:27:32 +0300

    Add story for custom icons (#474)

---
+++ src/stories/Icons.stories.tsx
@@ -0,0 +1,191 @@
+// Copyright (C) 2021 Cartesi Pte. Ltd.
+
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import {
+    DashboardIcon,
+    DelegateIcon,
+    PencilIcon,
+    PencilIconWhite,
+    CheckCircleIcon,
+    PoolBalanceIcon,
+    PoolAllowenceIcon,
+    AllowanceIcon,
+    WalletIcon,
+    TimerIcon,
+    TimeIcon,
+    StakeIcon,
+    StakedBalanceIcon,
+    EffectiveBalanceIcon,
+    PoolUsersIcon,
+    PoolProductionIntervalIcon,
+    PoolCommisionIcon,
+    DisconnectIcon,
+    SwitchIcon,
+    PaginationIcon,
+    ArrowsUpDownIcon,
+    CopyIcon,
+    PoolPerformanceIcon,
+    EyeIcon,
+    FilterIcon,
+    CloseIcon,
+    ViewMoreIcon,
+    PoolsTimer,
+    StakePlus,
+    StakeCircled,
+} from '../components/Icons';
+
+const Icons = () => (
+    <div
+        style={{
+            display: 'grid',
+            gridTemplateColumns: '50px 50px 50px 50px 50px 50px',
+            gridGap: 20,
+        }}
+    >
+        <div title="DashboardIcon">
+            <DashboardIcon width={10} height={10} />
+        </div>
+
+        <div title="DelegateIcon">
+            <DelegateIcon width={10} height={10} />
+        </div>
+
+        <div title="PencilIcon">
+            <PencilIcon width={10} height={10} />
+        </div>
+
+        <div title="PencilIconWhite">
+            <PencilIconWhite width={10} height={10} />
+        </div>
+
+        <div title="CheckCircleIcon">
+            <CheckCircleIcon width={10} height={10} />
+        </div>
+
+        <div title="PoolBalanceIcon">
+            <PoolBalanceIcon width={10} height={10} />
+        </div>
+
+        <div title="PoolAllowenceIcon">
+            <PoolAllowenceIcon width={10} height={10} />
+        </div>
+
+        <div title="AllowanceIcon">
+            <AllowanceIcon width={10} height={10} />
+        </div>
+
+        <div title="WalletIcon">
+            <WalletIcon width={10} height={10} />
+        </div>
+
+        <div title="TimerIcon">
+            <TimerIcon width={10} height={10} />
+        </div>
+
+        <div title="TimeIcon">
+            <TimeIcon width={10} height={10} />
+        </div>
+
+        <div title="StakeIcon">
+            <StakeIcon width={10} height={10} />
+        </div>
+
+        <div title="StakedBalanceIcon">
+            <StakedBalanceIcon width={10} height={10} />
+        </div>
+
+        <div title="EffectiveBalanceIcon">
+            <EffectiveBalanceIcon width={10} height={10} />
+        </div>
+
+        <div title="PoolUsersIcon">
+            <PoolUsersIcon width={10} height={10} />
+        </div>
+
+        <div title="PoolProductionIntervalIcon">
+            <PoolProductionIntervalIcon width={10} height={10} />
+        </div>
+
+        <div title="DelegateIcon">
+            <DelegateIcon width={10} height={10} />
+        </div>
+
+        <div title="PoolCommisionIcon">
+            <PoolCommisionIcon width={10} height={10} />
+        </div>
+
+        <div title="DisconnectIcon">
+            <DisconnectIcon width={10} height={10} />
+        </div>
+
+        <div title="SwitchIcon">
+            <SwitchIcon width={10} height={10} />
+        </div>
+
+        <div title="PaginationIcon">
+            <PaginationIcon width={10} height={10} />
+        </div>
+
+        <div title="ArrowsUpDownIcon">
+            <ArrowsUpDownIcon width={10} height={10} />
+        </div>
+
+        <div title="ArrowsUpDownIcon">
+            <CopyIcon width={10} height={10} />
+        </div>
+
+        <div title="PoolPerformanceIcon">
+            <PoolPerformanceIcon width={10} height={10} />
+        </div>
+
+        <div title="EyeIcon">
+            <EyeIcon width={10} height={10} />
+        </div>
+
+        <div title="FilterIcon">
+            <FilterIcon width={10} height={10} />
+        </div>
+
+        <div title="CloseIcon">
+            <CloseIcon width={10} height={10} />
+        </div>
+
+        <div title="ViewMoreIcon">
+            <ViewMoreIcon width={10} height={10} />
+        </div>
+
+        <div title="PoolsTimer">
+            <PoolsTimer width={10} height={10} />
+        </div>
+
+        <div title="StakePlus">
+            <StakePlus width={10} height={10} />
+        </div>
+
+        <div title="StakeCircled">
+            <StakeCircled width={10} height={10} />
+        </div>
+    </div>
+);
+
+export default {
+    title: 'Icons',
+    component: Icons,
+    argTypes: {},
+} as ComponentMeta<typeof Icons>;
+
+const Template: ComponentStory<typeof Icons> = (args) => <Icons {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
