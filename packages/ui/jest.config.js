ca306a865b28bea5eaae11b874df3fc0572def39
Neven Dyulgerov <neven.diulgerov@gmail.com>
2023-01-27 12:07:19 +0200

    refactor(packages/ui): Add jest config and packages

---
+++ packages/ui/jest.config.js
@@ -0,0 +1,58 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
+        '!<rootDir>/src/graphql/models.ts',
+        '!<rootDir>/src/stories/**',
+        '!<rootDir>/src/styles/**',
+        '!**/*.d.ts',
+        '!**/node_modules/**',
+    ],
+    coverageReporters: ['lcov', 'text-summary'],
+    coverageDirectory: '<rootDir>/coverage',
+    moduleNameMapper: {
+        // Handle CSS imports (with CSS modules)
+        // https://jestjs.io/docs/webpack#mocking-css-modules
+        '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+        // Handle CSS imports (without CSS modules)
+        '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+
+        // Handle image imports
+        // https://jestjs.io/docs/webpack#handling-static-assets
+        '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.js`,
+
+        // Handle module aliases
+        uuid: '<rootDir>/../../node_modules/uuid/dist/umd/uuid.min.js',
+    },
+    testMatch: [
+        '**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)',
+        '**/?(*.)+(spec|test).[jt]s?(x)',
+    ],
+    testPathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+        '<rootDir>/.next/',
+        '<rootDir>/.github/',
+        '<rootDir>/.storybook/',
+        '<rootDir>/.vscode/',
+        '<rootDir>/public/',
+    ],
+    testEnvironment: 'jsdom',
+    // Common setup you would usually add before each test.
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    transform: {
+        // Use babel-jest to transpile tests with the next/babel preset
+        // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+        '^.+\\.(js|jsx|ts|tsx)$': [
+            'babel-jest',
+            {
+                presets: ['next/babel'],
+                plugins: ['@babel/plugin-proposal-private-methods'],
+            },
+        ],
+    },
+    transformIgnorePatterns: [
+        '/node_modules/(?!(@web3-onboard))/',
+        '^.+\\.module\\.(css|sass|scss)$',
+    ],
+};
