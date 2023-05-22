6833a2bdc9626e50654e92fc2c3e4e5f61be93ba
Bruno Menezes <bruno.menezes@cartesi.io>
2023-05-22 19:26:22 +1200

    chore(staking): Add copyright disclaimer

--- apps/staking/__tests__/services/mocks.ts
+++ apps/staking/__tests__/services/mocks.ts
@@ -1,3 +1,14 @@
+// Copyright (C) 2023 Cartesi Pte. Ltd.
+
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
 import { ContractTransaction, Event } from 'ethers';
 import { mock } from 'jest-mock-extended';
 import { buildContractReceipt } from '../components/node/mocks';
