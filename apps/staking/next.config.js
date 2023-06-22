42f2876598b973bb8def954a17bc45062cc8eb8f
Bruno Menezes <brunodmenezes@gmail.com>
2023-06-22 19:07:14 +1200

    feature(staking): Add CSP to allow safe to work with localhost

--- apps/staking/next.config.js
+++ apps/staking/next.config.js
@@ -9,6 +9,29 @@ const modules = [
 ];
 const withTM = require('next-transpile-modules')(modules);
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';  
+  font-src 'self';
+  frame-ancestors 'self' https://app.safe.global;
+`;
+
 module.exports = withTM({
     reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/manifest.json',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: ContentSecurityPolicy.replace(
+                            /\s{2,}/g,
+                            ' '
+                        ).trim(),
+                    },
+                ],
+            },
+        ];
+    },
 });
