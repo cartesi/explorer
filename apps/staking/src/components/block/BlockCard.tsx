5b53fa7f9ca5862b5b22e293c5a02843c5e5298e
nevendiulgerov <neven.diulgerov@gmail.com>
2023-08-07 15:08:05 +0300

    feat(staking): Update blocks page ui

--- apps/staking/src/components/block/BlockCard.tsx
+++ apps/staking/src/components/block/BlockCard.tsx
@@ -30,15 +30,19 @@ export interface BlockCardProps extends StackProps {
 
 const BlockCard: FC<BlockCardProps> = (props) => {
     const { chainId, block, highlight, highlightColor, ...stackProps } = props;
-    const bg = useColorModeValue('white', 'gray.700');
+    const bg = useColorModeValue('white', 'dark.gray.tertiary');
+    const borderColor = useColorModeValue('gray.900', 'dark.gray.quaternary');
+    const boxShadow = useColorModeValue('md', 'none');
+
     return (
         <HStack
-            shadow="md"
+            shadow={boxShadow}
             p={4}
             bg={bg}
-            borderLeftWidth={10}
-            borderLeftColor="gray.900"
+            borderLeftWidth={16}
+            borderLeftColor={borderColor}
             align="flex-start"
+            borderRadius="1rem"
             {...stackProps}
         >
             <Box w="100%">
