f49ca96e0684930b675310e95601a89043367ff3
Bruno Menezes <brunodmenezes@gmail.com>
2024-02-10 10:54:20 +1300

    chore: Improve DX by augmenting TS intellisense.

--- apps/staking/additional.d.ts
+++ apps/staking/additional.d.ts
@@ -1,3 +1,17 @@
 interface Window {
     ethereum: any;
 }
+
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        NEXT_PUBLIC_ENVIRONMENT: string;
+        NEXT_PUBLIC_UNLEASH_PROXY_HOST: string;
+        NEXT_PUBLIC_UNLEASH_PROXY_CLIENT_KEY: string;
+        NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID: string;
+        NEXT_PUBLIC_PROJECT_ID: string;
+        NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: string;
+        NEXT_PUBLIC_MAINNET_GRAPHQL_URL: string;
+        NEXT_PUBLIC_GOERLI_GRAPHQL_URL: string;
+        NEXT_PUBLIC_SEPOLIA_GRAPHQL_URL: string;
+    }
+}
