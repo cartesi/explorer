de11111e5d8cdc8e2485f86fc48f555851d4d579
Bruno Menezes <brunodmenezes@gmail.com>
2024-12-19 17:19:17 +1300

    feat: Add drizzle kit and orm as new dependencies for staking. Map the env vars into turbo.json and ts declaration files.

--- apps/staking/additional.d.ts
+++ apps/staking/additional.d.ts
@@ -17,5 +17,33 @@ declare namespace NodeJS {
          * More info at {@link https://thegraph.com/explorer/subgraphs/5XqPmWe6gjyrJtFn9cLy237i4cWw2j9HcUJEXsP5qGtH?view=Query&chain=arbitrum-one}
          */
         ENS_GRAPHQL_URL: string;
+        /**
+         *  Storage to be used (e.g. ens information). It can have the following formats
+         *  remote service: libsql://{database-name}-{company-name}.turso.io or
+         *  local development: file:{path-to-your-local-file}.db
+         */
+        TURSO_DATABASE_URL: string;
+        /**
+         * Only necessary when using the Turso platform. For development
+         * with local db it is not necessary.
+         */
+        TURSO_AUTH_TOKEN?: string;
+
+        /**
+         * A node to fetch mainnet blockchain information. e.g. Alchemy / Infura.
+         */
+        HTTP_MAINNET_NODE_RPC: string;
+
+        /**
+         * should be a number that set when ENS information about an address
+         * is considered staled.
+         */
+        ENS_ENTRY_TTL: string;
+
+        /**
+         * An authorization header value passed down by a CRON service calling
+         * functions under /api/cron/*. It can be any agreed value.
+         */
+        CRON_SECRET: string;
     }
 }
