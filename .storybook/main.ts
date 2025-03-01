e20d8559636a5750fe82cab623c303388d50ef16
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-03-01 15:12:01 +0200

    feat: Fix issues

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
-const config = {
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
+
+const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
     addons: [
         'storybook-addon-apollo-client',
