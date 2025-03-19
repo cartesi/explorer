3aa696d8c12c489b1b0aab221c4df8799ad29761
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-03-19 23:21:49 +0200

    feat: Move commissions and stake pages under app router

---
+++ src/app/stake/[pool]/stake/page.tsx
@@ -0,0 +1,40 @@
+import {
+    getENSStaticProps,
+    getPoolsStaticPaths,
+} from '../../../../utils/staticGeneration';
+import { FC } from 'react';
+import { notFound } from 'next/navigation';
+import StakePoolStake from '../../../../components/stake/StakePoolStake';
+
+export const revalidate = 60 * 60 * 24;
+
+export async function generateStaticParams() {
+    return getPoolsStaticPaths();
+}
+
+interface StakePoolPageProps {
+    params: Promise<{ pool: string }>;
+}
+
+export async function generateMetadata(props: StakePoolPageProps) {
+    const params = await props.params;
+    const data = await getENSStaticProps({ params });
+
+    return {
+        title: `Stake to ${data.formattedAddress}`,
+        description: `Stake to ${data.formattedAddress}`,
+    };
+}
+
+const StakePoolStakePage: FC<StakePoolPageProps> = async (props) => {
+    const params = await props.params;
+    const data = await getENSStaticProps({ params });
+
+    if (data.notFound) {
+        notFound();
+    }
+
+    return <StakePoolStake />;
+};
+
+export default StakePoolStakePage;
