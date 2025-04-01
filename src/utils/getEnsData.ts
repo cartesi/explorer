09a97c9544e7d4c8a0a68c206bc60a443cad0c7f
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-04-01 11:10:50 +0300

    chore: Remove server action for getting ens data

--- src/utils/getEnsData.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import AddressENSService from '../services/server/ens/AddressENSService';
-
-export const getEnsData = async () => {
-    const result = await AddressENSService.listAll();
-    return result.ok ? result.data : [];
-};
