1e0650092fc5d83ec69d567e258f63790be72ef8
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-04-21 18:43:01 +0300

    fix: Fix typo

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -13,7 +13,7 @@
 
 import { Box, Heading, HStack, SimpleGrid } from '@chakra-ui/react';
 import { useState } from 'react';
-import BlockMiniCard from '../block/BlockMiniCard's;
+import BlockMiniCard from '../block/BlockMiniCard';
 import HomeHeader from './Header';
 import Stats from './Stats';
 import Layout from '../Layout';
