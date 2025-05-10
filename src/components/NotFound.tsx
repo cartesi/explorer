7a32e796436daea041804d8ee7fd536519cfdb19
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-05-10 14:33:34 +0300

    chore: Uncomment code

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,24 +1,23 @@
 'use client';
 
-// import { Heading, HStack, Text } from '@chakra-ui/react';
 import Layout from '../components/Layout';
+import { Heading, HStack, Text } from '@chakra-ui/react';
 
 export default function NotFound() {
     return (
         <Layout>
-            <p>not found</p>
-            {/*<HStack justifyContent="center" py="20vh">*/}
-            {/*    <Heading*/}
-            {/*        fontSize="3xl"*/}
-            {/*        fontWeight="bold"*/}
-            {/*        borderEndStyle="solid"*/}
-            {/*        borderEndWidth={1}*/}
-            {/*        paddingEnd={3}*/}
-            {/*    >*/}
-            {/*        404*/}
-            {/*    </Heading>*/}
-            {/*    <Text fontSize="lg">This page could not be found.</Text>*/}
-            {/*</HStack>*/}
+            <HStack justifyContent="center" py="20vh">
+                <Heading
+                    fontSize="3xl"
+                    fontWeight="bold"
+                    borderEndStyle="solid"
+                    borderEndWidth={1}
+                    paddingEnd={3}
+                >
+                    404
+                </Heading>
+                <Text fontSize="lg">This page could not be found.</Text>
+            </HStack>
         </Layout>
     );
 }
