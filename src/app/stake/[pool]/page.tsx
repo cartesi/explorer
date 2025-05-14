8295c43aa133f4bbea888c36f1fbab8dbcda5bd2
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-05-14 21:24:21 +0300

    feat: Retrieve the ENS names from ENS cache instead of stake pages metadata

--- src/app/stake/[pool]/page.tsx
+++ src/app/stake/[pool]/page.tsx
@@ -1,16 +1,8 @@
-import {
-    getENSStaticProps,
-    getPoolsStaticPaths,
-} from '../../../utils/staticGeneration';
 import { FC } from 'react';
 import StakePool from '../../../components/stake/StakePool';
 import { notFound } from 'next/navigation';
-
-export const revalidate = 86400;
-
-export async function generateStaticParams() {
-    return getPoolsStaticPaths();
-}
+import { ethers } from 'ethers';
+import { getFormattedEnsName } from '../../../services/server/utils';
 
 interface StakePoolPageProps {
     params: Promise<{ pool: string }>;
@@ -18,19 +10,24 @@ interface StakePoolPageProps {
 
 export async function generateMetadata(props: StakePoolPageProps) {
     const params = await props.params;
-    const data = await getENSStaticProps({ params });
+    const address = params.pool;
+
+    if (!ethers.utils.isAddress(address)) {
+        notFound();
+    }
+
+    const formattedAddress = getFormattedEnsName(address);
 
     return {
-        title: `Cartesi pool info - ${data.formattedAddress}`,
-        description: `Cartesi pool info - ${data.formattedAddress}`,
+        title: `Cartesi pool info - ${formattedAddress}`,
+        description: `Cartesi pool info - ${formattedAddress}`,
     };
 }
 
 const StakePoolPage: FC<StakePoolPageProps> = async (props) => {
     const params = await props.params;
-    const data = await getENSStaticProps({ params });
 
-    if (data.notFound) {
+    if (!ethers.utils.isAddress(params.pool)) {
         notFound();
     }
 
