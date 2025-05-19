0f78b15bc36a35c1f7c76b762c449d3a71c7fdd9
Bruno Menezes <brunodmenezes@gmail.com>
2025-05-19 19:21:00 +1200

    chore: Remove typescript additional declaration.

--- additional.d.ts
+++ additional.d.ts
@@ -5,14 +5,11 @@ interface Window {
 declare namespace NodeJS {
     export interface ProcessEnv {
         NEXT_PUBLIC_ENVIRONMENT: string;
-        NEXT_PUBLIC_UNLEASH_PROXY_HOST: string;
-        NEXT_PUBLIC_UNLEASH_PROXY_CLIENT_KEY: string;
         NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID: string;
         NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: string;
         NEXT_PUBLIC_MAINNET_GRAPHQL_URL: string;
         NEXT_PUBLIC_SEPOLIA_GRAPHQL_URL: string;
-        
-        
+
         NEXT_PUBLIC_FLAG_POS_V2_ENABLED: string;
         NEXT_PUBLIC_FLAG_ANKR_ENABLED: string;
         /**
