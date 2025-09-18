b070f79e23d9f2b3c05e7f487fcf98247f4f9319
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-09-18 11:09:45 +0300

    feat: Enable typed routes

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const ContentSecurityPolicy = `
 `;
 
 module.exports = {
+    typedRoutes: true,
     reactStrictMode: true,
     webpack: (config) => {
         config.resolve.fallback = { fs: false, net: false, tls: false };
