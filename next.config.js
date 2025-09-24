8acc56b0182308b21d3087c17146574c4aeae3c7
Neven Dyulgerov <neven.diulgerov@gmail.com>
2025-09-24 22:22:35 +0300

    chore: Remove obsolete webpack config

--- next.config.js
+++ next.config.js
@@ -8,11 +8,6 @@ const ContentSecurityPolicy = `
 module.exports = {
     typedRoutes: true,
     reactStrictMode: true,
-    webpack: (config) => {
-        config.resolve.fallback = { fs: false, net: false, tls: false };
-        config.externals.push('pino-pretty', 'encoding');
-        return config;
-    },
     async headers() {
         return [
             {
