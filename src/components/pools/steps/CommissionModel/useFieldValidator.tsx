d8e8f0aecf0d73ef9d443491591903309753e8b6
Bruno Menezes <brunodmenezes@gmail.com>
2025-10-07 15:43:30 +1300

    fix: Type inference compiler length limit. TS7056
    * Add a explicit type annotation to hint the compiler and avoid deep recursion to kick in on complex types.

--- src/components/pools/steps/CommissionModel/useFieldValidator.tsx
+++ src/components/pools/steps/CommissionModel/useFieldValidator.tsx
@@ -1,6 +1,16 @@
 import { isEmpty, isFunction } from 'lodash/fp';
 import { useEffect } from 'react';
-import { Path, RegisterOptions, useForm } from 'react-hook-form';
+import {
+    FieldErrors,
+    Path,
+    RegisterOptions,
+    useForm,
+    UseFormClearErrors,
+    UseFormGetValues,
+    UseFormRegisterReturn,
+    UseFormReset,
+    UseFormTrigger,
+} from 'react-hook-form';
 import { useMessages } from '../../../../utils/messages';
 import { ValidationResult } from '../../../BaseInput';
 
@@ -10,11 +20,29 @@ type UseFieldValidatorProps<T, V> = {
     onValidationChange?: (vr: ValidationResult<V>) => void;
 };
 
+/**
+ * Explicit type annotation to avoid TS7056 error due to
+ * compiler limits to infer the types on complex types.
+ * (ref: https://github.com/microsoft/TypeScript/issues/43817)
+ */
+type UseFieldValidator<FormValidatorType> = {
+    registerReturn: UseFormRegisterReturn<Path<FormValidatorType>>;
+    trigger: UseFormTrigger<FormValidatorType>;
+    errors: FieldErrors<FormValidatorType>;
+    reset: UseFormReset<FormValidatorType>;
+    clearErrors: UseFormClearErrors<FormValidatorType>;
+    isDirty: boolean;
+    getValue: () => ReturnType<UseFormGetValues<FormValidatorType>>[0];
+};
+
 const useFieldValidator = <FormValidatorType, ValidationResultType>({
     fieldName,
     options,
     onValidationChange,
-}: UseFieldValidatorProps<FormValidatorType, ValidationResultType>) => {
+}: UseFieldValidatorProps<
+    FormValidatorType,
+    ValidationResultType
+>): UseFieldValidator<FormValidatorType> => {
     const {
         register,
         formState: { errors, isDirty },
@@ -32,7 +60,7 @@ const useFieldValidator = <FormValidatorType, ValidationResultType>({
             message: useMessages('field.isRequired'),
         },
         ...options,
-    } as RegisterOptions);
+    } as RegisterOptions<FormValidatorType>);
 
     const inputErrors = errors[fieldName as string];
 
