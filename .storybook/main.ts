e2a7bcf31819f3619dc9e082c75af181f978fecd
Bruno Menezes <brunodmenezes@gmail.com>
2026-09-22 22:12:53 +0100

    chore: Bump Storybook to v10, Vite to v8 and the Chromatic toolchain.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-onboarding',
-        '@chromatic-com/storybook',
-    ],
+    addons: ['@storybook/addon-links', '@chromatic-com/storybook'],
     framework: {
         name: '@storybook/nextjs-vite',
         options: {},
